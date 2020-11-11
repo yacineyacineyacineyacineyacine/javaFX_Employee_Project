691eb7fa14d7e8f1f59cb1889351c3f5b088e09d
yacine <yacineyacine31212@gmail.com>
2020-11-11 12:03:22 +0100

    login finished

--- src/application/Login.java
+++ src/application/Login.java
@@ -1,12 +1,17 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class Login {
@@ -14,8 +19,14 @@ public class Login {
     @FXML
     Label dbConLb;
 
+    @FXML
+    TextField username;
 
+    @FXML
+    TextField password;
 
+    @FXML
+    Label check;
 
     public void statusDB(javafx.event.ActionEvent event) throws IOException, SQLException {
 
@@ -24,6 +35,21 @@ public class Login {
             }else{
                 dbConLb.setText("not connected!");
             }
-
     }
+
+     public void enterCP(ActionEvent event) throws IOException, SQLException{
+              List <Admins> list = AdminsDB.getAdmins();
+              Map<String, String> map = new HashMap<String, String>();
+              for(Admins a: list){
+                  map.put(a.getUsername(), a.getPassword());
+              }
+
+              if(map.containsKey(username.getText().trim())){
+                  String val2 = map.get(username.getText().trim());
+                  if(val2.equals(password.getText().trim()));
+                  check.setText("Success");
+              }else {
+                  check.setText("Failed try again");
+              }
+     }
 }

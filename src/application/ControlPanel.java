6ed5b968ae0a80035332b17c6dd6836ca77bfc2b
yacine <yacineyacine31212@gmail.com>
2020-11-12 13:19:56 +0100

    finish EmployeesView

--- src/application/ControlPanel.java
+++ src/application/ControlPanel.java
@@ -1,4 +1,49 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
 public class ControlPanel {
+
+    public void viewEmployee(ActionEvent event){
+        AdminsDB.getConnection();
+        Stage primaryStage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/application/ViewEmployees.fxml"));
+
+            Scene scene = new Scene(root,  992, 632);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void InsertEmployee(ActionEvent event){
+        AdminsDB.getConnection();
+        Stage primaryStage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/application/InsertEmployees.fxml"));
+
+            Scene scene = new Scene(root,  992, 632);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
--- src/application/ViewEmployees.java
+++ src/application/ViewEmployees.java
@@ -1,4 +1,78 @@
 package application;
 
-public class ViewEmployees {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ViewEmployees implements Initializable {
+    @FXML
+    TableView<Employee> table;
+    @FXML
+    TableColumn<Employee, Integer> id;
+    @FXML
+    TableColumn<Employee, String> fname;
+    @FXML
+    TableColumn<Employee, String> lname;
+    @FXML
+    TableColumn<Employee, Float> salary;
+    @FXML
+    TableColumn<Employee, Integer> age;
+    @FXML
+    TableColumn<Employee, String> department;
+    @FXML
+    TableColumn<Employee, String> address;
+    @FXML
+    TableColumn<Employee, String> date_of_birth;
+
+    public ObservableList<Employee> data = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        String sql = "SELECT * FROM `employee` WHERE 1";
+        Connection connection = DBInfo.getConnection();
+        try {
+            PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                data.add(new Employee(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getFloat(4),
+                        resultSet.getInt(5),
+                        resultSet.getString(6),
+                        resultSet.getString(7),
+                        resultSet.getString(8)
+                ));
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        id.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("id"));
+        fname.setCellValueFactory(new PropertyValueFactory<Employee, String>("fname"));
+        lname.setCellValueFactory(new PropertyValueFactory<Employee, String>("lname"));
+        salary.setCellValueFactory(new PropertyValueFactory<Employee, Float>("salary"));
+        age.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("age"));
+        department.setCellValueFactory(new PropertyValueFactory<Employee, String >("department"));
+        address.setCellValueFactory(new PropertyValueFactory<Employee, String>("address"));
+        date_of_birth.setCellValueFactory(new PropertyValueFactory<Employee, String>("date_of_birth"));
+
+        table.setItems(data);
+
+    }
 }
